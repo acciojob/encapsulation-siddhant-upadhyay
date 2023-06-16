6fe3a61b0ac2424f0986957171318b785d1db1e6
KIIT <siddhantupadhyay6@gmail.com>
2023-06-16 11:41:35 +0530

    code has been compleated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+
+        RWOnly obj = new RWOnly();
+
+        // try setting a value to name in Main
+        // function using setter function and access it's value using getter function
+        obj.setName("siddhant");
+        obj.getName();
+    }
+
 }
